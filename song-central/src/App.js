cb027583ce320d0c76b8df425989122415bd3f2d
Sami Samara <samisamara99@yahoo.com>
2023-08-20 23:24:31 -0500

    created components folder

--- song-central/src/App.js
+++ song-central/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Song Central</h1>
+      <Header />
     </div>
   );
 }
